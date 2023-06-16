bf311512ffa2dd757087bdd90ffc242054f98747
Afdhal Putra <74084602+afpury@users.noreply.github.com>
2023-06-16 20:40:56 +0700

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -8,23 +8,3 @@ for i in range(pass_length):
 
 print(password)
 
-a = "1"
-b = "2"
-print(a) 
-# a = a + b
-a += b
-print(a) 
-
-
-# import random
-
-# karakter = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-# pass_length = int(input("Berapa banyak karakter yang diingankan untuk passwordnya? "))
-
-# password = ""
-
-# for i in range(pass_length):
-#     password += random.choice(karakter)
-
-# print(password)
-
